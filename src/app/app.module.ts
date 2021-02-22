ca50feb12acb4d812ef67a30caaadf7a617367b3
masamichan <positivemasa031@gmail.com>
2021-02-22 16:23:57 +0900

    ダッシュボードの追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,16 @@ import { MembersComponent } from './members/members.component';
 import { MemberDetailComponent } from './member-detail/member-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [AppComponent, MembersComponent, MemberDetailComponent, MessagesComponent],
+  declarations: [
+    AppComponent,
+    MembersComponent,
+    MemberDetailComponent,
+    MessagesComponent,
+    DashboardComponent,
+  ],
   imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
