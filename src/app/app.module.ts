b689b61df523c3c232a6e5e57075792be5b34e45
masamichan <positivemasa031@gmail.com>
2021-02-22 12:20:37 +0900

    コンポーネントを分割する

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MembersComponent } from './members/members.component';
+import { MemberDetailComponent } from './member-detail/member-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, MembersComponent],
+  declarations: [AppComponent, MembersComponent, MemberDetailComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
