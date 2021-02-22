518ae2f0258e5ba8359eb50da586366245fce13b
masamichan <positivemasa031@gmail.com>
2021-02-22 13:52:29 +0900

    メッセージログを表示する

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MembersComponent } from './members/members.component';
 import { MemberDetailComponent } from './member-detail/member-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
-  declarations: [AppComponent, MembersComponent, MemberDetailComponent],
+  declarations: [AppComponent, MembersComponent, MemberDetailComponent, MessagesComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/member.service.ts
+++ src/app/member.service.ts
@@ -2,14 +2,16 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Member } from './member';
 import { MEMBERS } from './mock-members';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MemberService {
-  constructor() {}
+  constructor(private messageService: MessageService) {}
 
   getMembers(): Observable<Member[]> {
+    this.messageService.add('MemberService: 社員一覧データを取得しました');
     return of(MEMBERS);
   }
 }
